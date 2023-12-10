c7db060ccc7f4b81eb4dc188ebf3d64f7acbe001
samir <samir.casis@gmail.com>
2023-12-10 10:36:42 -0300

    funciones ok 90%, fatla actualizar valor en checkbox

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,9 @@
 const inputTarea = document.querySelector('input');
 const btnAgregar = document.querySelector('#agregar');
 const lista = document.querySelector('ul');
-const tareas = [ 
+const total = document.querySelector('#total');
+const realizadas = document.querySelector('#realizadas');
+const tareas = [
     { id: 1, nombre: 'Comprar en el super', completed: false },
     { id: 2, nombre: 'Estudiar para la prueba', completed: false },
     { id: 3, nombre: 'Pasear al perro', completed: false },
@@ -14,24 +16,38 @@ btnAgregar.addEventListener('click', () => {
     inputTarea.value = '';
 
     agregarALista(); // invoca a la función 
+    actualizar();
 });
 
 const agregarALista = () => {
-    lista.innerHTML = tareas.map((t) => 
+    lista.innerHTML = tareas.map((t) =>
         `<li>${t.id} ${t.nombre} <input type="checkbox" onclick="comprobar(${t.id})" ${t.completed ? 'checked' : ''}> <button onclick="borrar(${t.id})">X</button></li>`
     ).join('');
+    if (tareas.completed) {
+        tareas++;
+    }
+    
+    actualizar()
 };
 
-const borrar = (id) => { 
+const borrar = (id) => {
     const index = tareas.findIndex((t) => t.id === id);
     tareas.splice(index, 1);
     agregarALista();
+    actualizar();
 };
 
 const comprobar = (id) => {
     const tarea = tareas.find((t) => t.id === id);
     tarea.completed = !tarea.completed;
     agregarALista();
+    actualizar();
+};
+
+const actualizar = () => {
+    total.innerHTML = tareas.length;
+    const tareasRealizadas = tareas.findIndex((t) => t.completed).length;
+    realizadas.innerHTML = tareasRealizadas;
 };
 
 agregarALista();
